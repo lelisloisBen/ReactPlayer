931c17d28e7734b7eaa3b926a298bfbf070cc40b
VMORI0112 <victorfernan_moriganador@hotmail.com>
2019-10-17 16:08:13 +0000

    reactplayer commit4

--- react-player/src/components/player.js
+++ react-player/src/components/player.js
@@ -5,9 +5,13 @@ import './play.css';
 const PlayControl = () => {
 
     const [datas, setDatas] = useState(null);
+    const [myMusic, setMyMusic] = useState();
 
-const LoadDatas = () => {
-    fetch('https://assets.breatheco.de/apis/sound/songs')
+    var songURL = "";
+    var currentSongNum = 0;
+
+    const LoadDatas = () => {
+     fetch('https://assets.breatheco.de/apis/sound/songs')
         .then(function(response) {
             if (!response.ok) {
             throw Error(response.statusText);
@@ -21,23 +25,40 @@ const LoadDatas = () => {
         .catch(function(error) {
             console.log('Looks like there was a problem: \n', error);
         });
-}
+    }
 
-const Getplay = (e) => {
-    let songURL = "https://assets.breatheco.de/apis/sound/"+e.target.dataset.url;
-    console.log(songURL);
-}
+    const Getplay = (e) => {
+          songURL = "https://assets.breatheco.de/apis/sound/"+e.target.dataset.url;
+          currentSongNum = e.target.dataset.songnum;
+          setMyMusic(new Audio(songURL));
+          console.log(currentSongNum)
+    }
 
-useEffect(() => {
-    LoadDatas()
-},[])
+   // console.log(myMusic);
+   // console.log("num" + currentSongNum + songURL)
+
+    const PlaySong = () => {
+          myMusic.play();
+
+    }
+    const PauseSong = () => {
+          myMusic.pause();
+    }
+    const PreviousSong = () => {
+          myMusic.play();
+    }
+
+
+    useEffect(() => {
+         LoadDatas()
+    },[])
 
 	return (
        <>
           <div className="boxbtn mx-outo">
-            <button type="button" className="btn btn-outline-primary">Play</button>
-            <button type="button" className="btn btn-outline-danger">Pause</button>
-            <button type="button" className="btn btn-outline-secondary">Previous</button>
+            <button type="button" className="btn btn-outline-primary" onClick={PlaySong}>Play</button>
+            <button type="button" className="btn btn-outline-danger" onClick={PauseSong}>Pause</button>
+            <button type="button" className="btn btn-outline-secondary" onClick={PreviousSong}>Previous</button>
             <button type="button" className="btn btn-outline-success">Next</button>
 
           </div>
@@ -50,8 +71,9 @@ useEffect(() => {
                         className="list-group-item clickme"
                         onClick={Getplay}
                         data-url={item.url}
+                        data-songnum={index}
                     >
-                        {item.id} - {item.name} - URL = {item.url}
+                        {index + 1} - {item.name} - URL = {item.url}
                     </li>
                 )
 
