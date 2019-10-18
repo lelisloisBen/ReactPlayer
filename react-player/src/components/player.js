1e8ca5d0320a21c47e50eceeb1803a0475e9c834
samir benzada <samirbenzada@gmail.com>
2019-10-18 11:53:04 -0400

    update

--- react-player/src/components/player.js
+++ react-player/src/components/player.js
@@ -6,6 +6,7 @@ const PlayControl = () => {
 
     const [data, setData] = useState(null);
     const [myMusic, setMyMusic] = useState();
+    const [myMusicName, setMyMusicName] = useState();
     const [currentSongNum, setCurrentSongNum] = useState(0);
 
     var songURL = "";
@@ -20,7 +21,6 @@ const PlayControl = () => {
         })
         .then(function(responseAsJson) {
             setData(responseAsJson);
-            //console.log(responseAsJson);
         })
         .catch(function(error) {
             console.log('Looks like there was a problem: \n', error);
@@ -28,100 +28,87 @@ const PlayControl = () => {
     }
 
     const Getplay = (event) => {
-          songURL = "https://assets.breatheco.de/apis/sound/"+event.target.dataet.url;
-          //currentSongNum = event.target.dataet.songnum;
-          setCurrentSongNum(event.target.dataet.songnum);
-          //setCurrentSongNum(event.url);
-          setMyMusic(new Audio(songURL));
-
+        songURL = "https://assets.breatheco.de/apis/sound/"+event.target.dataset.url;
+        setCurrentSongNum(event.target.dataset.songnum);
+        setMyMusic(new Audio(songURL));
+        setMyMusicName(event.target.dataset.name);
     }
- //console.log("here samir "+currentSongNum);
-   // console.log(myMusic);
-   // console.log("num" + currentSongNum + songURL)
-
     const PlaySong = () => {
         myMusic.pause();
         myMusic.currentTime = 0;
         myMusic.play();
-
     }
     const PauseSong = () => {
         myMusic.pause();
         myMusic.currentTime = 0;
         myMusic.pause();
     }
-
     const PreviousSong = () => {
-
           setCurrentSongNum(currentSongNum - 1);
-
           if (currentSongNum === 0) {
              let lastIndex = data.length-1;
              songURL =  "https://assets.breatheco.de/apis/sound/"+ data[lastIndex].url;
              setCurrentSongNum(lastIndex);
-             //console.log(currentSongNum + "Last Index" + lastIndex + songURL);
              setMyMusic(new Audio(songURL));
+             setMyMusicName(data[lastIndex].name);
              PauseSong();
           }else{
              songURL =  "https://assets.breatheco.de/apis/sound/"+ data[currentSongNum].url;
              setMyMusic(new Audio(songURL));
+             setMyMusicName(data[currentSongNum].name);
              PauseSong();
-              //console.log(currentSongNum +"previous" +songURL);
           }
-          //console.log(currentSongNum);
     }
-    //console.log("previous" + currentSongNum);
-
     const NextSong = () => {
           setCurrentSongNum(currentSongNum + 1);
-
           if (currentSongNum < data.length - 1 ) {
               songURL =  "https://assets.breatheco.de/apis/sound/"+ data[currentSongNum].url;
               setMyMusic(new Audio(songURL));
-              //console.log(currentSongNum + "Last Index" + (data.length - 1)  + songURL);
+              setMyMusicName(data[currentSongNum].name);
               PauseSong();
           }else{
               let firstindex = 1;
               songURL =  "https://assets.breatheco.de/apis/sound/"+ data[firstindex].url;
               setMyMusic(new Audio(songURL));
-              //console.log(currentSongNum +"next" +songURL);
+              setMyMusicName(data[firstindex].name);
               PauseSong();
           }
     }
-
-    //console.log("next" + currentSongNum);
-
     useEffect(() => {
          LoadData()
-    },[])
-
+    },[myMusic]);
 	return (
        <>
-          <div className="boxbtn mx-outo">
-            <button type="button" className="btn btn-outline-primary" onClick={PlaySong}>Play</button>
-            <button type="button" className="btn btn-outline-danger" onClick={PauseSong}>Pause</button>
-            <button type="button" className="btn btn-outline-secondary" onClick={PreviousSong}>Previous</button>
-            <button type="button" className="btn btn-outline-success" onClick={NextSong}>Next</button>
+        <div className="boxbtn mx-outo text-center">
+            <button type="button" className="btn btn-outline-primary m-3" onClick={PlaySong}>Play</button>
+            <button type="button" className="btn btn-outline-danger m-3" onClick={PauseSong}>Pause</button>
+            <button type="button" className="btn btn-outline-secondary m-3" onClick={PreviousSong}>Previous</button>
+            <button type="button" className="btn btn-outline-success m-3" onClick={NextSong}>Next</button>
+        </div>
+        <div className="container">
+            <div class="alert alert-primary my-3" role="alert">
+                {myMusicName}
+            </div>
+            <div>
+                <ol className="list-group">
+                    { !data ? ("Loading...") : data.map(( item, index) => {
+                        return (
+                            <li
+                                key={index}
+                                className="list-group-item clickme"
+                                onClick={Getplay}
+                                data-name={item.name}
+                                data-url={item.url}
+                                data-songnum={index}
+                            >
+                                {index + 1} - {item.name} - URL = {item.url}
+                            </li>
+                        )
 
-          </div>
-          <div>
-            <ol className="list-group">
-            { !data ? ("Loading...") : data.map(( item, index) => {
-                return (
-                    <li
-                        key={index}
-                        className="list-group-item clickme"
-                        onClick={Getplay}
-                        data-url={item.url}
-                        data-songnum={index}
-                    >
-                        {index + 1} - {item.name} - URL = {item.url}
-                    </li>
-                )
-
-            } ) }
-            </ol>
-          </div>
+                    } ) }
+                </ol>
+            </div>
+        </div>
       </>
 	);
 
